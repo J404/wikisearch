9d6d7f89653711f32dc2f6499d96e0a914ee1603
Jimmy <jimmystry@gmail.com>
2020-08-22 21:26:34 -0400

    Server can now run and serve react's build folder when in production mode

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var Express = require("express");
+var Path = require("path");
+require('dotenv').config();
 var app = Express();
 var port = (process.env.PRODUCTION === 'false') ? 5000 : 3000;
-app.get('/', function (req, res) {
-    res.send('yay server');
-});
+if (process.env.PRODUCTION === 'true') {
+    app.use(Express.static(Path.join(__dirname, './build')));
+}
 app.listen(port);
 console.log("Server is listening on port " + port);
--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import * as Express from 'express';
+import * as Path from 'path';
+
+require('dotenv').config();
 
 const app: Express.Application = Express();
 
 const port: number = (process.env.PRODUCTION === 'false') ? 5000 : 3000;
 
-app.get('/', (req: Express.Request, res: Express.Response): void => {
-    res.send('yay server');
-});
+if (process.env.PRODUCTION === 'true') {
+    app.use(Express.static(Path.join(__dirname, './build')));
+}
 
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
