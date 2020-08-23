26885e4e4c7ca6a94f7f000fd575cccfdd3110ac
Jimmy <jimmystry@gmail.com>
2020-08-22 21:37:44 -0400

    Moved server files to server folder for better org

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-"use strict";
+
 exports.__esModule = true;
 var Express = require("express");
 var Path = require("path");
--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const app: Express.Application = Express();
 const port: number = (process.env.PRODUCTION === 'false') ? 5000 : 3000;
 
 if (process.env.PRODUCTION === 'true') {
-    app.use(Express.static(Path.join(__dirname, './build')));
+    app.use(Express.static(Path.join(__dirname, '../build')));
 }
 
 app.listen(port);
